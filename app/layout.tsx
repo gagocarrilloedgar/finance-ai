3f5140bfd946b0ebcfd35ff83237bb0ea2e590a1
gagocarrilloedgar <edgargc.upc@gmail.com>
2025-02-17 23:24:52 +0100

    chore: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Toaster } from "sonner";
 import { Providers } from "../components/Providers";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Providers>
           {children}
           <SpeedInsights />
+          <Analytics />
           <Toaster />
         </Providers>
       </body>
